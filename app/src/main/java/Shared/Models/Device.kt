58c187bf43fe77076060f18c5979a30873819707
solderq35 <solderq35@gmail.com>
2022-10-27 19:49:46 -0700

    class update for device

--- app/src/main/java/Shared/Models/Device.kt
+++ app/src/main/java/Shared/Models/Device.kt
@@ -1,4 +1,12 @@
 package Shared.Models
 
 class Device {
+
+    private lateinit var id: String;
+    private lateinit var name: String;
+    private lateinit var active: Boolean;
+    private lateinit var wifi: String;
+    private lateinit var deviceType: String;
+    private lateinit var groups: List <String>;
+
 }
